d3325b3c53ef25e045378fb9e791de432a971dd2
26Vilela05 <victor.vilela011@gmail.com>
2025-10-02 18:43:00 -0300

    primeiro commit

---
+++ src/main/java/com/itb/inf2fm/innova/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.itb.inf2fm.innova.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;  
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer{
+    //Configurações de CORS podem ser adicionadas aqui no futuro
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+       
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173\", \"http://localhost:8686") // Permitir todas as origens (ajuste conforme necessário)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT", "PATCH");
+              
+    }
+
+}
